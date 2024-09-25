da230da573d81eb7aac99d2d30137a1683f7ca90
Niels Ringler <niels.ringler@br.de>
2024-09-25 11:43:55 +0200

    Fix port

--- app.py
+++ app.py
@@ -191,4 +191,4 @@ def extract_article_from_url(url):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=4000, log_config=LOGGING_CONFIG)
+    uvicorn.run(app, host="0.0.0.0", port=3000, log_config=LOGGING_CONFIG)
